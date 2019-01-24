9f53cb4a9041f5297d44cd195acf49487105ea64
Anupom Roy <anupomr@yahoo.com>
2019-01-24 15:52:44 -0500

    Menu is created

---
+++ app/src/main/java/com/example/anupo/seboapp/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.anupo.seboapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/anupo/seboapp/MainActivity.java
+++ app/src/main/java/com/example/anupo/seboapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.anupo.seboapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.home:
+                Toast.makeText(this, "You selected start!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.login:
+                Toast.makeText(this, "You selected stop!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.book:
+                Toast.makeText(this, "You selected help!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.exit:
+                Toast.makeText(this, "You selected help!", Toast.LENGTH_LONG).show();
+                System.exit(1);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
